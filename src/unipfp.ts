b66d2798e59ef44ce7e44e75061a399fc15e164e
uynilo9 <uynilo9@outlook.com>
2025-12-23 17:55:24 +0800

    refactor: rewrite the CLI and use factory functions for updates
    Rewrite the CLI.
    Use factory functions, `prepareUpdaterViaApi`, `prepareUpdaterViaBrowser`, `getUpdaterViaApi`, and `getUpdaterViaBrowser` for updates to reduce duplicate code and keep it clear.

--- src/unipfp.ts
+++ src/unipfp.ts
@@ -2,9 +2,9 @@ import * as fs from "@std/fs";
 
 import { getImageType } from "./utils/image.ts";
 import { createBrowser } from "./utils/browser.ts";
-import { updatePfpViaApi, updatePfpViaBrowser } from "./utils/update.ts";
+import { prepareUpdaterViaApi, prepareUpdaterViaBrowser } from "./utils/update.ts";
 
-import * as clack from "@clack/prompts";
+import { cancel, confirm, intro, isCancel, log, multiselect, outro, spinner, text } from "@clack/prompts";
 
 import Discord from "./plats/discord.ts";
 import GitHub from "./plats/github.ts";
@@ -17,211 +17,106 @@ import Threads from "./plats/threads.ts";
 import Twitch from "./plats/twitch.ts";
 import TwitterX from "./plats/twitterx.ts";
 
-async function main() {
-	clack.intro("Unipfp");
+intro("Unipfp");
 
-	const image = await clack.text({
-		message: "Where's your pfp located?",
-		placeholder: "./pfp.jpg",
-		validate: (value) => {
-			if (!value) {
-				return "Please enter a path.";
-			}
+const image = await text({
+	message: "Where is your new pfp located?",
+	validate: (value) => {
+		if (!value) {
+			return "Please enter a path.";
+		}
 
-			const type = getImageType(value);
-			if (type.isErr()) {
-				return type.error.message;
-			}
+		const type = getImageType(value);
+		if (type.isErr()) {
+			return type.error.message;
+		}
 
-			if (!fs.existsSync(value)) {
-				return "Please enter a valid image file path.";
-			}
-		},
-	});
-	if (clack.isCancel(image)) {
-		clack.cancel("Operation cancelled.");
-		Deno.exit(1);
-	}
-
-	const platforms = await clack.multiselect({
-		message: "Which platforms do you want to update your pfp?",
-		options: [
-			{ value: "discord", label: "Discord", hint: "via browser" },
-			{ value: "github", label: "GitHub", hint: "via browser" },
-			{ value: "gitlab", label: "GitLab", hint: "via API" },
-			{ value: "instagram", label: "Instagram", hint: "via browser" },
-			{ value: "plex", label: "Plex", hint: "via browser" },
-			{ value: "reddit", label: "Reddit", hint: "via browser" },
-			{ value: "steam", label: "Steam", hint: "via browser" },
-			{ value: "threads", label: "Threads", hint: "via browser" },
-			{ value: "twitch", label: "Twitch", hint: "via browser" },
-			{ value: "twitterx", label: "Twitter(X)", hint: "via browser" },
-		],
-		required: true,
-	});
-	if (clack.isCancel(platforms)) {
-		clack.cancel("Operation cancelled.");
-		Deno.exit(1);
-	}
-
-	if (platforms.includes("discord")) {
-		clack.log.warn("You may have to manually bypass Discord reCAPTCHA, as it sometimes shows up.");
-	}
-	if (platforms.includes("reddit")) {
-		clack.log.warn("You have to click the Reddit logo in the top left corner to bypass Reddit reCAPTCHA while status check.");
-	}
-	if (platforms.includes("steam")) {
-		clack.log.warn("You may have to manually comfirm your login on your Steam mobile app.");
-	}
-	if (platforms.includes("threads")) {
-		clack.log.warn("You must fill in your Instagram username and password since you've selected Threads.");
-	}
-
-	const comfirm = await clack.confirm({
-		message: "Are you sure to update your pfp?",
-		active: "Yes",
-		inactive: "No",
-	});
-	if (clack.isCancel(comfirm) || !comfirm) {
-		clack.cancel("Operation cancelled.");
-		Deno.exit(1);
-	}
-
-	await fs.ensureDir("cookies");
-
-	const browser = await createBrowser();
-	if (browser.isErr()) {
-		clack.log.error(browser.error.message);
-		Deno.exit(1);
-	}
-
-	await clack.tasks([
-		{
-			enabled: platforms.includes("discord"),
-			title: "Starting to update your Discord pfp",
-			task: async () => {
-				const discord = await updatePfpViaBrowser(browser.value, Discord, image);
-				if (discord.isErr()) {
-					return discord.error.message;
-				}
-
-				return discord.value;
-			},
-		},
-		{
-			enabled: platforms.includes("github"),
-			title: "Starting to update your GitHub pfp",
-			task: async () => {
-				const github = await updatePfpViaBrowser(browser.value, GitHub, image);
-				if (github.isErr()) {
-					return github.error.message;
-				}
-
-				return github.value;
-			},
-		},
-		{
-			enabled: platforms.includes("gitlab"),
-			title: "Starting to update your GitLab pfp",
-			task: async () => {
-				const gitlab = await updatePfpViaApi(GitLab, image);
-				if (gitlab.isErr()) {
-					return gitlab.error.message;
-				}
-
-				return gitlab.value;
-			},
-		},
-		{
-			enabled: platforms.includes("instagram"),
-			title: "Starting to update your Instagram pfp",
-			task: async () => {
-				const instagram = await updatePfpViaBrowser(browser.value, Instagram, image);
-				if (instagram.isErr()) {
-					return instagram.error.message;
-				}
-
-				return instagram.value;
-			},
-		},
-		{
-			enabled: platforms.includes("plex"),
-			title: "Starting to update your Plex pfp",
-			task: async () => {
-				const plex = await updatePfpViaBrowser(browser.value, Plex, image);
-				if (plex.isErr()) {
-					return plex.error.message;
-				}
-
-				return plex.value;
-			},
-		},
-		{
-			enabled: platforms.includes("reddit"),
-			title: "Starting to update your Reddit pfp",
-			task: async () => {
-				const reddit = await updatePfpViaBrowser(browser.value, Reddit, image);
-				if (reddit.isErr()) {
-					return reddit.error.message;
-				}
-
-				return reddit.value;
-			},
-		},
-		{
-			enabled: platforms.includes("steam"),
-			title: "Starting to update your Steam pfp",
-			task: async () => {
-				const steam = await updatePfpViaBrowser(browser.value, Steam, image);
-				if (steam.isErr()) {
-					return steam.error.message;
-				}
-
-				return steam.value;
-			},
-		},
-		{
-			enabled: platforms.includes("threads"),
-			title: "Starting to update your Threads pfp",
-			task: async () => {
-				const threads = await updatePfpViaBrowser(browser.value, Threads, image);
-				if (threads.isErr()) {
-					return threads.error.message;
-				}
-
-				return threads.value;
-			},
-		},
-		{
-			enabled: platforms.includes("twitch"),
-			title: "Starting to update your Twitch pfp",
-			task: async () => {
-				const twitch = await updatePfpViaBrowser(browser.value, Twitch, image);
-				if (twitch.isErr()) {
-					return twitch.error.message;
-				}
-
-				return twitch.value;
-			},
-		},
-		{
-			enabled: platforms.includes("twitterx"),
-			title: "Starting to update your Twitter(X) pfp",
-			task: async () => {
-				const twitterx = await updatePfpViaBrowser(browser.value, TwitterX, image);
-				if (twitterx.isErr()) {
-					return twitterx.error.message;
-				}
-
-				return twitterx.value;
-			},
-		},
-	]);
-
-	await browser.value.close();
-
-	clack.outro("Missions completed.");
+		if (!fs.existsSync(value)) {
+			return "Please enter a valid image file path.";
+		}
+	},
+});
+if (isCancel(image)) {
+	cancel("Mission cancelled.");
 	Deno.exit(0);
 }
 
-await main();
+const platforms = await multiselect({
+	message: "Which platforms do you want to update your pfp?",
+	options: [
+		{ value: "discord", label: "Discord", hint: "via browser" },
+		{ value: "github", label: "GitHub", hint: "via browser" },
+		{ value: "gitlab", label: "GitLab", hint: "via API" },
+		{ value: "instagram", label: "Instagram", hint: "via browser" },
+		{ value: "plex", label: "Plex", hint: "via browser" },
+		{ value: "reddit", label: "Reddit", hint: "via browser" },
+		{ value: "steam", label: "Steam", hint: "via browser" },
+		{ value: "threads", label: "Threads", hint: "via browser" },
+		{ value: "twitch", label: "Twitch", hint: "via browser" },
+		{ value: "twitterx", label: "Twitter(X)", hint: "via browser" },
+	],
+	required: true,
+});
+if (isCancel(platforms)) {
+	cancel("Mission cancelled.");
+	Deno.exit(0);
+}
+if (platforms.includes("discord")) {
+	log.warn("You may have to manually bypass Discord reCAPTCHA, as it sometimes shows up.");
+}
+if (platforms.includes("reddit")) {
+	log.warn("You have to click the Reddit logo in the top left corner to bypass Reddit reCAPTCHA while status check.");
+}
+if (platforms.includes("steam")) {
+	log.warn("You may have to manually comfirm your login on your Steam mobile app.");
+}
+if (platforms.includes("threads")) {
+	log.warn("You must fill in your Instagram username and password since you've selected Threads.");
+}
+
+const comfirm = await confirm({
+	message: "Are you sure to update your pfp?",
+});
+if (isCancel(comfirm) || !comfirm) {
+	cancel("Mission cancelled.");
+	Deno.exit(0);
+}
+
+await fs.ensureDir("cookies");
+
+const browser = await createBrowser();
+if (browser.isErr()) {
+	log.error(browser.error.message);
+	Deno.exit(1);
+}
+
+const getUpdaterViaApi = prepareUpdaterViaApi(image);
+const getUpdaterViaBrowser = prepareUpdaterViaBrowser(browser.value, image);
+const updater = {
+	discord: getUpdaterViaBrowser(Discord),
+	github: getUpdaterViaBrowser(GitHub),
+	gitlab: getUpdaterViaApi(GitLab),
+	instagram: getUpdaterViaBrowser(Instagram),
+	plex: getUpdaterViaBrowser(Plex),
+	reddit: getUpdaterViaBrowser(Reddit),
+	steam: getUpdaterViaBrowser(Steam),
+	threads: getUpdaterViaBrowser(Threads),
+	twitch: getUpdaterViaBrowser(Twitch),
+	twitterx: getUpdaterViaBrowser(TwitterX),
+};
+
+for (const platform of platforms) {
+	const s = spinner();
+	s.start(`Starting to update pfp on ${platform}`);
+
+	const result = await updater[platform]();
+	if (result.isErr()) {
+		s.error(result.error.message);
+		Deno.exit(1);
+	}
+
+	s.stop(result.value);
+}
+
+outro("Mission completed.");
+Deno.exit(0);
--- src/utils/update.ts
+++ src/utils/update.ts
@@ -3,53 +3,61 @@ import * as fs from "@std/fs";
 import { Err, Ok } from "../types.ts";
 import type { Browser, BrowserContext, PlatformViaApi, PlatformViaBrowser, Result } from "../types.ts";
 
-export async function updatePfpViaApi(platform: PlatformViaApi, image: string): Promise<Result<string>> {
-    const pfpUpdated = await platform.performUpdate(image);
-    if (pfpUpdated.isErr()) {
-        return Err(pfpUpdated.error);
-    }
+export function prepareUpdaterViaApi(image: string): (platform: PlatformViaApi) => () => Promise<Result<string>> {
+	return (platform) => {
+		return async () => {
+			const pfpUpdated = await platform.performUpdate(image);
+			if (pfpUpdated.isErr()) {
+				return Err(pfpUpdated.error);
+			}
 
-    return Ok(`Successfully updated pfp on ${platform.name}.`);
+			return Ok(`Successfully updated pfp on ${platform.name}.`);
+		};
+	};
 }
 
-export async function updatePfpViaBrowser(browser: Browser, platform: PlatformViaBrowser, image: string): Promise<Result<string>> {
-    let context: BrowserContext;
+export function prepareUpdaterViaBrowser(browser: Browser, image: string): (platform: PlatformViaBrowser) => () => Promise<Result<string>> {
+	return (platform) => {
+		return async () => {
+			let context: BrowserContext;
 
-    if (await fs.exists(platform.cookiesPath)) {
-        context = await browser.newContext({ storageState: platform.cookiesPath });
-    } else {
-        context = await browser.newContext();
-    }
+			if (await fs.exists(platform.cookiesPath)) {
+				context = await browser.newContext({ storageState: platform.cookiesPath });
+			} else {
+				context = await browser.newContext();
+			}
 
-    const page = await context.newPage();
+			const page = await context.newPage();
 
-    const accountLoggedIn = await platform.checkStatus(page);
-    if (accountLoggedIn.isErr()) {
-        await page.close();
-        await context.close();
-        return Err(accountLoggedIn.error);
-    }
+			const accountLoggedIn = await platform.checkStatus(page);
+			if (accountLoggedIn.isErr()) {
+				await page.close();
+				await context.close();
+				return Err(accountLoggedIn.error);
+			}
 
-    if (accountLoggedIn.isOk() && !accountLoggedIn.value) {
-        const loggingIn = await platform.performLogin(page);
-        if (loggingIn.isErr()) {
-            await page.close();
-            await context.close();
-            return Err(loggingIn.error);
-        }
-    }
+			if (accountLoggedIn.isOk() && !accountLoggedIn.value) {
+				const loggingIn = await platform.performLogin(page);
+				if (loggingIn.isErr()) {
+					await page.close();
+					await context.close();
+					return Err(loggingIn.error);
+				}
+			}
 
-    await context.storageState({ path: platform.cookiesPath });
+			await context.storageState({ path: platform.cookiesPath });
 
-    const pfpUpdated = await platform.performUpdate(page, image);
-    if (pfpUpdated.isErr()) {
-        await page.close();
-        await context.close();
-        return Err(pfpUpdated.error);
-    }
+			const pfpUpdated = await platform.performUpdate(page, image);
+			if (pfpUpdated.isErr()) {
+				await page.close();
+				await context.close();
+				return Err(pfpUpdated.error);
+			}
 
-    await page.close();
-    await context.close();
+			await page.close();
+			await context.close();
 
-    return Ok(`Successfully updated pfp on ${platform.name}.`);
+			return Ok(`Successfully updated pfp on ${platform.name}.`);
+		};
+	};
 }
